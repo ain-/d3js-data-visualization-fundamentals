83a6fa9d807ddfb796d0117bf7be6c7f2a3386ff
Ain <ain.salula@gmail.com>
2017-01-06 21:03:22 +0200

    colorPicker

--- charting.js
+++ charting.js
@@ -6,6 +6,11 @@ var svg = d3.select("body").append("svg")
             .attr("width", w)
             .attr("height", h);
 
+function colorPicker(v) {
+    if (v <= 20) { return "#666666"; }
+    else if (v > 20) { return "#FF0033"; }
+}
+
 svg.selectAll("rect")
     .data(dataset)
     .enter()
@@ -15,5 +20,5 @@ svg.selectAll("rect")
             y: function(d) { return h - (d * 4); },
             width: w / dataset.length - padding,
             height: function(d) { return d * 4; },
-            fill: function(d) { return "rgb(" + (d*10) + ", 0, 0)"; }
+            fill: function(d) { return colorPicker(d); }
         });
