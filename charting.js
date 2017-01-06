d3c0e4a1d0642f7f66103831edb35e9dcae693e1
Ain <ain.salula@gmail.com>
2017-01-06 21:13:36 +0200

    adding data as labels

--- charting.js
+++ charting.js
@@ -1,7 +1,8 @@
 var w = 300;
 var h = 100;
 var padding = 2;
-var dataset = [5, 10, 14, 20, 25];
+var dataset = [5, 10, 14, 20, 25,
+               11, 25, 22, 18, 7];
 var svg = d3.select("body").append("svg")
             .attr("width", w)
             .attr("height", h);
@@ -22,3 +23,18 @@ svg.selectAll("rect")
             height: function(d) { return d * 4; },
             fill: function(d) { return colorPicker(d); }
         });
+
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function (d) { return d; })
+    .attr({
+        "text-anchor": "middle",
+        x: function(d, i) { return i * (w / dataset.length) + 
+            (w / dataset.length - padding) / 2; },
+        y: function(d) { return h - (d * 4) + 14; },
+        "font-family": "sans-serif",
+        "font-size": 12,
+        "fill": "#ffffff"
+    });        
