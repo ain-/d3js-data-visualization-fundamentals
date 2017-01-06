c15c7b4d3c98cd71abac5bcb5b0e8875686c2e74
Ain <ain.salula@gmail.com>
2017-01-06 19:56:52 +0200

    basic data usage example

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-d3.select("body")
-    .append("svg")
-        .attr("width", 250)
-        .attr("height", 50)
-    .append("text")
-        .text("Easy Peasy")
-        .attr("y", 25)
+var w = 500;
+var h = 100;
+var padding = 2;
+var dataset = [5, 10, 15, 20, 25];
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+            
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
         .attr("x", 0)
-        .style("fill", "blue");
+        .attr("y", 0)
+        .attr("width", 20)
+        .attr("height", 100);
