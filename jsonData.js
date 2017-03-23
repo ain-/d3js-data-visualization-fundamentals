19e183d2f3686d9844024e2d841ba75b829384b4
Ain <ain.salula@gmail.com>
2017-03-23 15:36:23 +0200

    load json data

---
+++ jsonData.js
@@ -0,0 +1,31 @@
+var h = 100;
+var w = 400;
+var ds;
+var metrics = [];
+
+function buildLine() {
+    var lineFun = d3.svg.line()
+      .x(function(d) { return ((d.month - 20160001) / 3.25); })
+      .y(function(d) { return h-d.sales; })
+      .interpolate("linear");
+
+    var svg = d3.select("body").append("svg").attr({ width: w, height: h});
+
+    var viz = svg.append("path").attr({
+      d: lineFun(ds),
+      "stroke": "purple",
+      "stroke-width": 2,
+      "fill": "none"
+    });
+}
+
+d3.json("MonthlySales.json", function(error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+    ds = data;
+  }
+  buildLine();
+
+});
