130c58ac593b9b9c3d05bbffaa2da982f04f5e3c
Ain <ain.salula@gmail.com>
2017-03-23 10:23:24 +0200

    csv graph

---
+++ csvData.js
@@ -0,0 +1,27 @@
+var h = 100;
+var w = 400;
+var ds;
+
+d3.csv("MonthlySales.csv", function(error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+    ds = data;
+  }
+
+  var lineFun = d3.svg.line()
+    .x(function(d) { return ((d.month - 20160001) / 3.25); })
+    .y(function(d) { return h-d.sales; })
+    .interpolate("linear");
+
+  var svg = d3.select("body").append("svg").attr({ width: w, height: h});
+
+  var viz = svg.append("path").attr({
+    d: lineFun(ds),
+    "stroke": "purple",
+    "stroke-width": 2,
+    "fill": "none"
+  });
+
+});
