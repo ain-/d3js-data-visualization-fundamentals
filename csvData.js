b6516834c53524b2a2a39599f90f93384fa04301
Ain <ain.salula@gmail.com>
2017-03-23 12:10:18 +0200

    extract line building

--- csvData.js
+++ csvData.js
@@ -2,6 +2,22 @@ var h = 100;
 var w = 400;
 var ds;
 
+function buildLine() {
+    var lineFun = d3.svg.line()
+      .x(function(d) { return ((d.month - 20160001) / 3.25); })
+      .y(function(d) { return h-d.sales; })
+      .interpolate("linear");
+
+    var svg = d3.select("body").append("svg").attr({ width: w, height: h});
+
+    var viz = svg.append("path").attr({
+      d: lineFun(ds),
+      "stroke": "purple",
+      "stroke-width": 2,
+      "fill": "none"
+    });
+}
+
 d3.csv("MonthlySales.csv", function(error, data) {
   if (error) {
     console.log(error);
@@ -9,19 +25,6 @@ d3.csv("MonthlySales.csv", function(error, data) {
     console.log(data);
     ds = data;
   }
-
-  var lineFun = d3.svg.line()
-    .x(function(d) { return ((d.month - 20160001) / 3.25); })
-    .y(function(d) { return h-d.sales; })
-    .interpolate("linear");
-
-  var svg = d3.select("body").append("svg").attr({ width: w, height: h});
-
-  var viz = svg.append("path").attr({
-    d: lineFun(ds),
-    "stroke": "purple",
-    "stroke-width": 2,
-    "fill": "none"
-  });
+  buildLine();
 
 });
