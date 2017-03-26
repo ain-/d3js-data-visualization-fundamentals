993367bed644d76e638997172dc1b4ef60b96380
Ain <ain.salula@gmail.com>
2017-03-26 20:09:56 +0300

    add css

---
+++ axis.js
@@ -0,0 +1,45 @@
+var h = 100;
+var w = 300;
+
+function buildLine(ds) {
+
+    var xScale = d3.scale.linear()
+      .domain([
+        d3.min(ds, d => d.month),
+        d3.max(ds, d => d.month)
+      ])
+      .range([0, w]);
+
+    var yScale = d3.scale.linear()
+      .domain([
+        0, d3.max(ds, d => d.sales)])
+      .range([h, 0]);
+
+    var lineFun = d3.svg.line()
+      .x(function(d) { return xScale(d.month); })
+      .y(function(d) { return yScale(d.sales); })
+      .interpolate("linear");
+
+    var svg = d3.select("body").append("svg").attr({ width: w, height: h});
+
+    var viz = svg.append("path").attr({
+      d: lineFun(ds),
+      "stroke": "purple",
+      "stroke-width": 2,
+      "fill": "none"
+    });
+}
+
+d3.json("https://api.github.com/repos/ain-/d3js-data-visualization-fundamentals/contents/MonthlySales.json", function(error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+    ds = data;
+  }
+  var decodedData = JSON.parse(window.atob(data.content));
+  console.log(decodedData);
+
+  buildLine(decodedData);
+
+});
