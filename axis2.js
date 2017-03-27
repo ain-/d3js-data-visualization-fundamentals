99a8d0d788ce1c85af9e6c9788fc4597d075ba0c
Ain <ain.salula@gmail.com>
2017-03-27 10:48:50 +0300

    add x-axis

---
+++ axis2.js
@@ -0,0 +1,56 @@
+var h = 100;
+var w = 300;
+var padding = 20;
+
+function buildLine(ds) {
+
+    var xScale = d3.scale.linear()
+      .domain([
+        d3.min(ds, d => d.month),
+        d3.max(ds, d => d.month)
+      ])
+      .range([padding+5, w-padding]);
+
+    var yScale = d3.scale.linear()
+      .domain([
+        0, d3.max(ds, d => d.sales)])
+      .range([h - padding, 10]);
+
+    var xAxisGen = d3.svg.axis().scale(xScale).orient("bottom")
+    var yAxisGen = d3.svg.axis().scale(yScale).orient("left").ticks(4);
+
+    var lineFun = d3.svg.line()
+      .x(function(d) { return xScale(d.month); })
+      .y(function(d) { return yScale(d.sales); })
+      .interpolate("linear");
+
+    var svg = d3.select("body").append("svg").attr({ width: w, height: h});
+
+    var yAxis = svg.append("g").call(yAxisGen)
+      .attr("class", "axis")
+      .attr("transform", "translate(" + padding + ", 0)");
+    var xAxis = svg.append("g").call(xAxisGen)
+      .attr("class", "axis")
+      .attr("transform", "translate(0,"+ (h-padding) + ")");
+
+    var viz = svg.append("path").attr({
+      d: lineFun(ds),
+      "stroke": "purple",
+      "stroke-width": 2,
+      "fill": "none"
+    });
+}
+
+d3.json("https://api.github.com/repos/ain-/d3js-data-visualization-fundamentals/contents/MonthlySales.json", function(error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+    ds = data;
+  }
+  var decodedData = JSON.parse(window.atob(data.content));
+  console.log(decodedData);
+
+  buildLine(decodedData);
+
+});
